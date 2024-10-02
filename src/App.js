acd01c6ece7238b4751fb2a6102b2183395c72f7
Miguel <miguel.rosa.goncalves@gmail.com>
2024-10-02 11:51:15 -0300

    fix: expose commit ref and build id on the ui

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Test
+          Learn React Test Build ID: {process.env.REACT_APP_NETLIFY_BUILD_ID}
+          Commit Ref:{process.env.REACT_APP_NETLIFY_COMMIT_REF}
         </a>
       </header>
     </div>
